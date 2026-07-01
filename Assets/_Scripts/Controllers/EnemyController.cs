3ac6feb18b346f6aa03fc9d39d0c0dbc2a8480c6
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2026-07-01 18:29:00 +0900

    EnemyController 스크립트 > Patrol() 수정(웨이포인트 순환)
    EnemyController 스크립트의 Patrol 함수를 수정하였음
    1. 웨이포인트에 도달하면(0.5f이하의 거리) 웨이포인트 인덱스 +1만큼 상승
    2. 마지막 인덱스에 도달하면 처음 인덱스로 돌아가 무한 반복

--- Assets/_Scripts/Controllers/EnemyController.cs
+++ Assets/_Scripts/Controllers/EnemyController.cs
@@ -13,19 +13,30 @@ public class EnemyController : MonoBehaviour
 
     void Patrol()
     {
-        if(waypoints.Length == 0) return;
+        if (waypoints.Length == 0) return;
 
-        //현재 위치
+        // 현재 목적지의 위치 좌표
         Vector3 targetPositions = waypoints[_currentWaypointIndex].position;
 
-        //움직일 방향 (목적지 - 현재 위치)
+        // 움직일 방향 (목적지 - 현재 위치)
         Vector3 direction = targetPositions - transform.position;
-        direction.y = 0;
+
+        targetPositions.y = transform.position.y;
+        direction = targetPositions - transform.position;
 
         float speed = 4.0f;
         transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
 
-        //next
+        // 웨이포인트와의 거리
+        float distanceToTarget = Vector3.Distance(transform.position, targetPositions);
+
+        // 도착 판정 범위
+        if (distanceToTarget < 0.5f)
+        {
+            _currentWaypointIndex++;
+
+            if (_currentWaypointIndex >= waypoints.Length)
+                _currentWaypointIndex = 0;
+        }
     }
-    
 }
