53e0ffc3a900e8d11b4a25fb42c2735da63b1b23
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2026-07-06 19:24:06 +0900

    Door의 상호작용 방식 변경
    상호작용이 마친 뒤 문을 순간이동 된 기존의 방식과 달리, 부드럽게 스르륵 열릴 수 있게 변경함.
    
    - TargetOffset을 통해 문의 최종 위치값을 조절할 수 있음
    - Open Speed를 조절하여 문이 열리는 속도를 조절할 수 있음.

--- Assets/_Scripts/Contents/Interact/DoorObject.cs
+++ Assets/_Scripts/Contents/Interact/DoorObject.cs
@@ -2,32 +2,63 @@ using UnityEngine;
 
 public class DoorObject : Interactable
 {
-    private bool _isOpened = false;
+    [Header("열릴 때 목표 위치 (상대값)")]
+    [SerializeField] private Vector3 _targetOffset = new Vector3(0,3f,0);
+
+    [Header("열리는 속도")]
+    [SerializeField] private float _openSpeed = 2f;
+
+    private bool _isOpened = false; //문이 열리는 중인지
+    private Vector3 _startPosition; // 초기 위치
+    private Vector3 _endPosition; // 최종 위치
+
 
     void Awake()
     {
         interactionPrompt = "HOLD [E] TO OPEN THE DOOR";
         RequiredInteractionTime = 3.0f;
+
+        _startPosition = transform.position;
     }
 
     public override void OnInteractComplete()
     {
         if (_isOpened) return;
 
-        base.OnInteractComplete();
+        //.OnInteractComplete();
 
-        Debug.Log("OPEN DOOR");
+        _endPosition = _startPosition + _targetOffset;
         _isOpened = true;
 
-        //임시 위치 조정
-        transform.position += new Vector3(0, 3.0f, 0);
+        Debug.Log("문이 열렸다.");
 
         //열린 문은 상호작용 더이상 불가
         if (GetComponent<Collider>() != null)
         {
             GetComponent<Collider>().enabled = false;
         }
-
-
     }
+
+    void Update()
+    {
+        if(_isOpened)
+        {
+            //문을 일정 속도로 열기
+            transform.position = Vector3.MoveTowards(
+                transform.position,         //현재 문의 위치
+                _endPosition,               //최종 문의 위치
+                _openSpeed * Time.deltaTime //속도
+            );
+
+
+            //현재 위치 == 최종 위치
+            if (transform.position == _endPosition)
+            {
+                _isOpened = false;
+                Debug.Log("문이 완전 열렸습니다.");
+            }
+        }
+    }
+
+
 }
