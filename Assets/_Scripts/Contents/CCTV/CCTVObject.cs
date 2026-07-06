e87a814eb78b84d7e1696cf950a470ef7a42e1d8
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2026-07-07 03:26:34 +0900

    Physics.Raycast 기반 CCTV 시선 차단(장애물 엄폐) 시스템 구현
    -부채꼴 시야각을 통과한 플레이어를 대상으로 Physics.Raycast를 발사.
    
    -중간에 Obstacle(장애물) 레이어를 가진 벽이 가로막으면 시선이 차단되도록 처리하여 벽 뒤에 숨는 '엄폐 기능'의 논리적 토대 완성.
    
    -OnDrawGizmos를 활용해 시선이 닿을 때는 빨간색, 차단될 때는 초록색 선으로 씬 창에 시각화하여 디버깅 환경 구축.

--- Assets/_Scripts/Contents/CCTV/CCTVObject.cs
+++ Assets/_Scripts/Contents/CCTV/CCTVObject.cs
@@ -7,6 +7,10 @@ public class CCTVObject : MonoBehaviour
 {
     [Header("감시 설정")]
     [SerializeField] private float _viewAngle = 90f; // 시야각
+    [SerializeField] private float _viewDistance = 7.0f; //최대 감시거리(Raycast_레이저 쏘는 거리)
+
+    [Header("레이어 설정")]
+    [SerializeField] private LayerMask _targetAndObstacleMask; //.검사할 레이어(플레이어 + Obstacle)
 
     private Transform _playerTransform; //감지된 플레이어의 위치를 기억할 상자
     private bool _isPlayerInSight = false; //플레이어가 시야 안에 있는지의 여부
@@ -39,6 +43,7 @@ public class CCTVObject : MonoBehaviour
     }
 
     //거리에 들어온 플레이어가 정면 '시야각' 안에도 있는지 계산하는 함수
+    //Raycast(레이저)
     private void CheckPlayerVisibility()
     {
         if (_playerTransform == null) return;
@@ -49,25 +54,51 @@ public class CCTVObject : MonoBehaviour
         // CCTV 정면 방향 ~ 플레이어 방향 사이의 '사이 각도' 
         float angleBetween = Vector3.Angle(transform.forward, directionToPlayer);
 
-        //시야각/2 <= '사이 각도' 
-        if(angleBetween <= _viewAngle * 0.5f)
+        //시야각/2 <= '사이 각도'안에 들어왔다면? (=> cctv 시야에 노출)
+        //Raycast 적용
+        if (angleBetween <= _viewAngle * 0.5f)
         {
-            //시야각 안으로 처음 들어왔을 때만 로그처리
-            if(!_isPlayerInSight)
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, directionToPlayer, out hit, _viewDistance, _targetAndObstacleMask))
             {
-                _isPlayerInSight = true;
-                Debug.Log("[경고] 플레이어가 CCTV의 부채꼴 시야에 노출되었습니다.");
+                if (hit.collider.CompareTag("Player"))
+                {
+                    if (!_isPlayerInSight)
+                    {
+                        _isPlayerInSight = true;
+                        Debug.Log("플레이어가 발각!!");
+                    }
+                }
+
+                //레이저에 가장 먼저 도달한게 Obstacle 레이어이라면?
+                else
+                {
+                    if (_isPlayerInSight)
+                    {
+                        _isPlayerInSight = false;
+                        Debug.Log("플레이어가 시야각 안에 있지만, 장애물 뒤에 엄폐되어 발각안됨");
+                    }
+                }
             }
         }
+        //시야각 안에 들어오지 않은 상태
         else
         {
-            if(_isPlayerInSight)
+            if (_isPlayerInSight)
             {
                 _isPlayerInSight = false;
-                Debug.Log("감지 해제");
+                Debug.Log("플레이어가 CCTV의 사각지대로 이동했습니다.");
             }
         }
+    }
 
+    private void OnDrawGizoms()
+    {
+        if(_playerTransform != null)
+        {
+            Gizmos.color = _isPlayerInSight ? Color.red : Color.green;
+            Gizmos.DrawLine(transform.position, _playerTransform.position);
+        }
     }
 
 
